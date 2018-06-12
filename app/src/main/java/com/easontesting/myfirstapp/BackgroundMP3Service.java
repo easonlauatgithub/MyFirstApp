15a60172c71b9fe49595c5e12a571a7b2046a4f1
Eason Lau <easonlauathkec@gmail.com>
2018-06-12 14:12:41 +0800

    Commit24: Started Service - MP3 enhancements

--- app/src/main/java/com/easontesting/myfirstapp/BackgroundMP3Service.java
+++ app/src/main/java/com/easontesting/myfirstapp/BackgroundMP3Service.java
@@ -1,16 +1,36 @@
 package com.easontesting.myfirstapp;
 
 import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.util.Log;
 
 public class BackgroundMP3Service extends IntentService {
+    private String DEBUG_TAG = this.getClass().getSimpleName();
+    public void funcDebug(){
+        Log.w(DEBUG_TAG,Thread.currentThread().getStackTrace()[3].getMethodName());
+    }
+
+    public static final String MP3_CODE = "MP3 Code";
+    public static final String ACTION_CODE = "ACTION Code";
+    MediaPlayer mediaPlayer;
     public BackgroundMP3Service() {
         super("BackgroundMP3Service");
     }
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.mp3_aboy_gdragon);
+    protected void onHandleIntent(Intent intent) {    }
+    public int onStartCommand (Intent intent, int flags,int startId){
+        funcDebug();
+        int mp3Code = intent.getIntExtra(MP3_CODE, -1);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), mp3Code);
         mediaPlayer.start();
+        return Service.START_STICKY;
+    }
+    @Override
+    public void onDestroy(){
+        funcDebug();
+        Log.w(DEBUG_TAG,"mediaPlayer: "+mediaPlayer);
+        mediaPlayer.stop();
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/easontesting/myfirstapp/StartedServiceActivity.java
+++ app/src/main/java/com/easontesting/myfirstapp/StartedServiceActivity.java
@@ -3,31 +3,48 @@ package com.easontesting.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Spinner;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Delayed;
 
 public class StartedServiceActivity extends AppCompatActivity {
+    private String DEBUG_TAG = this.getClass().getSimpleName();
+    public void funcDebug() {
+        Log.w(DEBUG_TAG, Thread.currentThread().getStackTrace()[3].getMethodName());
+    }
 
+    Intent intent_mp3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        funcDebug();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_started_service);
+        intent_mp3 = new Intent(this, BackgroundMP3Service.class);
     }
-    public void onCreateDelayedMessageService(View v){
+    public void onCreateDelayedMessageService(View v) {
         Intent i = new Intent(this, DelayedMessageService.class);
         i.putExtra(DelayedMessageService.EXTRA_MESSAGE, getResources().getString(R.string.started_service_btn_response));
         startService(i);
     }
-    public void onCreateBackgroundMP3(View v){
-        Intent i = new Intent(this, BackgroundMP3Service.class);
-        switch (v.getId()) {
-            case R.id.started_service_btn_start_mp3:
-                startService(i);
-                break;
-            case R.id.started_service_btn_stop_mp3:
-                stopService(i);
-                break;
-        }
+    public void onCreateBackgroundMP3(View v) {
+        funcDebug();
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Yoga Lin", R.raw.mp3_yogalin);
+        map.put("Jay Chow", R.raw.mp3_jaychow);
+        map.put("G Dragon", R.raw.mp3_aboy_gdragon);
+        Spinner spinnerURL=(Spinner) findViewById(R.id.started_service_mp3_lists);
+        String strMP3 = spinnerURL.getSelectedItem().toString();
+        int idMP3 = map.get(strMP3);
+        intent_mp3.putExtra(BackgroundMP3Service.MP3_CODE, idMP3);
+        stopService(intent_mp3);
+        startService(intent_mp3);
+    }
+    public void onStopBackgroundMP3(View v) {
+        funcDebug();
+        stopService(intent_mp3);
     }
 }
